a403188dc468b6f699df845e16c191dacb1a4130
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2018-08-05 12:28:59 +0700

    Simple add item

--- front/src/pages/inbox.rs
+++ front/src/pages/inbox.rs
@@ -3,11 +3,13 @@ use yew::services::ConsoleService;
 
 pub struct Model {
     items: Vec<String>,
+    adding_item: String,
     console: ConsoleService
 }
 
 pub enum Msg {
     Add,
+    AddingItemChanged(String),
     MarkAsTodo,
     MarkAsDone,
     Delete,
@@ -20,6 +22,7 @@ impl Component for Model {
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
         Model {
             items: vec![],
+            adding_item: String::new(),
             console: ConsoleService::new()
         }
     }
@@ -29,7 +32,16 @@ impl Component for Model {
             Msg::Add => {
                 self.console.log(&format!("Adding todo"));
 
-                false
+                self.items.push(self.adding_item.clone());
+                self.adding_item = String::new();
+
+                true
+            }
+            Msg::AddingItemChanged(new_adding_item) => {
+                self.console.log(&format!("Adding item changed to {}", new_adding_item));
+                self.adding_item = new_adding_item;
+
+                true
             }
             Msg::MarkAsTodo => {
                 self.console.log(&format!("Marking item as todo"));
@@ -54,16 +66,32 @@ impl Component for Model {
     }
 }
 
+fn view_item(item: &str) -> Html<Model> {
+    html! {
+        <div>
+            { item }
+        </div>
+    }
+}
+
 impl Renderable<Model> for Model {
     fn view(&self) -> Html<Self> {
         html! {
             <div>
+                <div>
+                    <input
+                        value=&self.adding_item,
+                        oninput=|e| Msg::AddingItemChanged(e.value),/>
+                </div>
+
                 <div>
                     <button onclick=|_| Msg::Add,>{ "Add" }</button>
                     <button onclick=|_| Msg::MarkAsTodo,>{ "Mark as todo" }</button>
                     <button onclick=|_| Msg::MarkAsDone,>{ "Mark as done" }</button>
                     <button onclick=|_| Msg::Delete,>{ "Delete" }</button>
                 </div>
+
+                { for self.items.iter().map(|item| view_item(item)) }
             </div>
         }
     }
